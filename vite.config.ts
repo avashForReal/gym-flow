118b78e6fa0155f9a332725b596bf75741051cfc
avashForReal <avash700@gmail.com>
2025-10-15 09:05:34 +0545

    alias fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
@@ -12,39 +12,40 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // VitePWA({
-    //   registerType: "autoUpdate", // update SW in background
-    //   workbox: {
-    //     globPatterns: ["**/*"], // cache everything built in /dist
-    //     cleanupOutdatedCaches: true,
-    //   },
-    //   manifest: {
-    //     name: "GYMFLOW",
-    //     short_name: "GYMFLOW",
-    //     description: "GYMFLOW",
-    //     theme_color: "#000000",
-    //     background_color: "#000000",
-    //     display: "standalone",
-    //     start_url: "/",
-    //     orientation: "portrait",
-    //     icons: [
-    //       { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
-    //       { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
-    //       {
-    //         src: "pwa-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "maskable",
-    //       },
-    //     ],
-    //   },
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    // }),
+    VitePWA({
+      registerType: "autoUpdate", // update SW in background
+      workbox: {
+        globPatterns: ["**/*"], // cache everything built in /dist
+        cleanupOutdatedCaches: true,
+      },
+      manifest: {
+        name: "GYMFLOW",
+        short_name: "GYMFLOW",
+        description: "GYMFLOW",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
+      'src': '/src',
       "@": path.resolve(process.cwd(), "src"),
     },
   },
